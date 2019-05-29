27000399c490a2e395cec37e50995a3e86f15480
agustinluna <agustinluna23@gmail.com>
2019-05-29 20:30:59 -0300

    comienzo de tanques

---
+++ Assets/Scripts/ElementalActivator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementalActivator : MonoBehaviour
+{
+    public float amountToFill;
+    float amount = 0;
+    public bool filled;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (amount > amountToFill)
+        {
+            filled = true;
+        }
+    }
+}
